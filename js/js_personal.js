1b836401efb8167c2257fbbef4c5cfc67e5e50e4
gustavoprieto <noticiasgus@gmail.com>
2022-12-04 11:11:02 +0100

    UPDATE info

--- js/js_personal.js
+++ js/js_personal.js
@@ -2,3 +2,11 @@
 $(function(){
     $("body").hide().fadeIn(3000);
 });
+
+let image = document.querySelector('#image-1');
+        let image2 = document.querySelector('#image-2');
+        
+        window.addEventListener('mousemove', function(e){
+            let pixel = e.clientX + 'px';
+            image.style.width = pixel
+        })
